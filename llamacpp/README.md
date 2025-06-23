d2395dae37786a9ce8a55caf32b942d299c209d6
Bobchenyx <chen.yixia@northeastern.edu>
2025-06-23 10:38:48 -0700

    add llamacpp

---
+++ llamacpp/README.md
@@ -0,0 +1,3 @@
+## Inference with llama.cpp
+
+
---
+++ llamacpp/snapshot_download.py
@@ -0,0 +1,18 @@
+# !pip install huggingface_hub hf_transfer
+import os
+# os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+from huggingface_hub import snapshot_download
+
+print("Starting download...")
+
+snapshot_download(
+    repo_id = "moxin-org/Moxin-7B-VLM",
+    local_dir = "Moxin-7B-VLM",
+)
+
+snapshot_download(
+    repo_id = "bobchenyx/Moxin-7B-VLM-hf",
+    local_dir = "Moxin-7B-VLM-hf",
+)
+
+print("Download finished.")
